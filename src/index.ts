0dae261690758795364354562902a85149cf16d2
joegutid <jose.gutierrez@i3sistemas.com>
2024-12-20 18:58:18 +0100

    fix: refactor: register

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
-import register from './rutas/x/shop/register'
-import purchase from './rutas/x/shop/register'
+import register from './rutas/x/bot/register'
+import purchase from './rutas/x/shop/purchase'
 import redsys from './rutas/x/payment/redsys'
 
 const app = new Hono()
@@ -9,7 +9,7 @@ app.get('/', (c) => {
   return c.text('Hello!!')
 })
 
-app.route('/x/shop', register)
+app.route('/x/bot', register)
 app.route('/x/shop', purchase)
 app.route('/x/payment', redsys)
 
