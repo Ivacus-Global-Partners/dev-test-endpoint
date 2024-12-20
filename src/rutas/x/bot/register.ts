e5363f65932cf9449e452a11390a3ff60e621be9
joegutid <jose.gutierrez@i3sistemas.com>
2024-12-20 21:58:51 +0100

    fix: register payload

--- src/rutas/x/bot/register.ts
+++ src/rutas/x/bot/register.ts
@@ -25,7 +25,7 @@ app.post('/register', async (c) => {
   const payload = await c.req.json()
   console.log(payload)
 
-  return c.json({ success: true, ...payload })
+  return c.json({ success: true, payload })
 })
 
 export default app
