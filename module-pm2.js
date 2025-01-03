c017a86ffd39e1712a633ab1de8ee2f1027cdc59
joegutid <jose.gutierrez@i3sistemas.com>
2025-01-03 10:46:51 +0100

    fix: module-pm2

---
+++ module-pm2.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'test-dev', // Name of your application
+  script: '/home/apps/dev-test-endpoint/built/index.js', // Entry point of your application
+  interpreter: '/root/.bun/bin/bun', // Bun interpreter
+}
