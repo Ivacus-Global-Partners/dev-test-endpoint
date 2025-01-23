1021567339bf7d608e13f04d6231905e717032d9
joegutid <jose.gutierrez@i3sistemas.com>
2025-01-23 12:44:25 +0100

    config: pm2 script

--- module-pm2.js
+++ module-pm2.js
@@ -3,3 +3,5 @@ module.exports = {
   script: '/home/apps/dev-test-endpoint/built/index.js', // Entry point of your application
   interpreter: '/root/.bun/bin/bun', // Bun interpreter
 }
+//para iniciar el proceso ejecutar este comando
+//pm2 start module-pm2.js
