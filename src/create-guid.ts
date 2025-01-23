1342338f779310970981069f7f9f87a29d876886
joegutid <jose.gutierrez@i3sistemas.com>
2025-01-23 12:46:19 +0100

    feat: script para crear guids manualmente

---
+++ src/create-guid.ts
@@ -0,0 +1,7 @@
+import { createGUID } from '@lib/cripto'
+
+const purchaseGUID = createGUID()
+const petpasspurchaseGUID = createGUID()
+
+console.log({ purchaseGUID })
+console.log({ petpasspurchaseGUID })
