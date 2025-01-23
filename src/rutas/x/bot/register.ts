0ef5d3f2296afa4678f658112ed5a68ee184d97c
joegutid <jose.gutierrez@i3sistemas.com>
2025-01-23 12:50:48 +0100

    feat: endpoint notifications: fixed

--- src/rutas/x/bot/register.ts
+++ src/rutas/x/bot/register.ts
@@ -9,4 +9,11 @@ app.post('/register', async (c) => {
   return c.json({ success: true, payload })
 })
 
+app.post('/notifications', async (c) => {
+  const payload = await c.req.json()
+  console.log(payload)
+
+  return c.json({ success: true, payload })
+})
+
 export default app
