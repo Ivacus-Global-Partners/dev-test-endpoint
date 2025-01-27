afe7b87de1d1406acc63c938fda3e652e5a87b7b
joegutid <jose.gutierrez@i3sistemas.com>
2025-01-28 00:52:18 +0100

    feat: datablock - testing

---
+++ src/rutas/x/bot/datablock.ts
@@ -0,0 +1,19 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/datablock', async (c) => {
+  const payload = await c.req.json()
+  console.log(payload)
+
+  return c.json({ success: true, payload })
+})
+
+// app.post('/notifications', async (c) => {
+//   const payload = await c.req.json()
+//   console.log(payload)
+
+//   return c.json({ success: true, payload })
+// })
+
+export default app
