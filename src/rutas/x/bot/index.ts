d866c7c007ebd656d8bb682f23642ee6c2592f8a
joegutid <jose.gutierrez@i3sistemas.com>
2025-01-28 03:25:06 +0100

    feat: bot: datablock endpoint

--- src/rutas/x/bot/index.ts
+++ src/rutas/x/bot/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
 import notifications from './notifications'
 import register from './register'
+import datablock from './datablock'
 
 const app = new Hono()
 
 app.route('/register', register)
+app.route('/datablock', datablock)
 app.route('/notifications', notifications)
 
 export default app
